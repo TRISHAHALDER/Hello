08e59664da2fc27c13b353ab3639eeb1964a30e3
Trisha <trishah.ug21.cs@nitp.ac.in>
2022-11-13 13:45:57 +0530

    chocolate.java modified and commited

--- chocolate.java
+++ chocolate.java
@@ -1,7 +1,7 @@
 class chocolate{
 
 public static void main(String[] args) {
-    System.out.println("Some chocolate added");
+    System.out.println("Some chocolate added,want to see the use case of git diff");
 }
 
 
